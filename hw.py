b6c5e1c6f80d6908ab9bca30abb6b30892d1afa9
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-31 16:04:47 -0400

    ready for review

--- hw.py
+++ hw.py
@@ -1,6 +1,5 @@
 
 from flask import Flask
-import random
 from flask import jsonify
 import records
 app = Flask(__name__)
@@ -8,12 +7,6 @@ app = Flask(__name__)
 db = records.Database("postgres://localhost/clemson_stats_db")
 
 @app.route("/")
-def hello():
-
-    return jsonify("Hello!")
-
-@app.route("/data")
-def get_rows(db):
+def get_rows():
     rows = db.query("SELECT * FROM clemson_stats_db;")
-    return rows
-    print(rows.export('json'))
+    return jsonify(rows.export('json'))
